113a8cfb4480caf54cbe62b4e3b8fcbf5b69c4a2
dmerniestic1987 <dmernies@uade.edu.ar>
2019-03-12 15:07:19 -0300

    Se agregan pantallas

--- CardPublicity/app/src/main/java/ar/com/betex/cardpublicity/MainActivity.java
+++ CardPublicity/app/src/main/java/ar/com/betex/cardpublicity/MainActivity.java
@@ -3,7 +3,12 @@ package ar.com.betex.cardpublicity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -36,9 +41,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this.onNavigationItemSelectedListener);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottonNavigation);
+        navigation.setOnNavigationItemSelectedListener(this.onButtonNavigationViewItemSelectedListener);
     }
 
+    //Esto es el mensaje para el Button Navigation Item
+    private BottomNavigationView.OnNavigationItemSelectedListener onButtonNavigationViewItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_dashboard:
+                    return true;
+                case R.id.navigation_notifications:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private NavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new  NavigationView.OnNavigationItemSelectedListener(){
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+
+            } else if (id == R.id.nav_my_cars) {
+
+            }
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
+    };
 }
